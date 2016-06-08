83d1d05792920460118d7532e086a8282ea23b84
Ben Carter <bcarter@redventures.com>
2016-06-08 15:37:09 -0400

    updates location

--- index.js
+++
@@ -1,3 +0,0 @@
-export { default as FullStory } from './lib/component/fullstory';
-export { default as FullStoryClient } from './lib/client';
-
---
+++ src/index.js
@@ -0,0 +1,3 @@
+export { default as FullStory } from './component/fullstory';
+export { default as FullStoryClient } from './client';
+
